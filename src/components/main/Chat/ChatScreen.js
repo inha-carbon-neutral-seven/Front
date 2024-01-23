5e67df323e58e1e539e681aa43c66ae0670f5c58
Choi BoKeun <cbk121779@gmail.com>
2024-01-23 18:36:06 +0900

    [최보근] 초기 안내 화면 추가

--- src/components/main/Chat/ChatScreen.js
+++ src/components/main/Chat/ChatScreen.js
@@ -3,12 +3,12 @@ import React, { useEffect } from "react";
 import ChatLogs from "./ChatLogs";
 import UserInput from "../Input/UserInput";
 import { addToChatLog } from "../../../reducers/chatReducers";
-import beaver from "../../../image/logo.jpg";
 import {
   clearSentMessage,
   clearAIAnswer,
   markMessagesOld,
 } from "../../../reducers/chatReducers";
+import InitialGuide from "./InitialGuide";
 
 function ChatScreen() {
   // App의 상태변수
@@ -49,41 +49,8 @@ function ChatScreen() {
     // overflow-auto
     <div className="flex-grow flex flex-col bg-white w-full h-full drop-shadow-lg max-h-[90vh] rounded-[12px] overflow-hidden">
       <div className="overflow-y-auto">
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          {
-            // 초기 대기화면
-            currentState === "init" && (
-              <div className="bg-gray-200 shadow-xl w-auto h-auto rounded-[74px] p-10 font-['BMHANNAPro']">
-                <p className="font-bold text-2xl">
-                  <img
-                    src={beaver}
-                    className="h-20 w-20 rounded-full mx-auto"
-                  />
-                  비버.ai 사용법
-                </p>
-                <p className="">
-                  비버는 일반적인 챗봇🤖 기능을 제공하며, 파일을 첨부하면 파일
-                  종류에 따라 질문응답이 가능합니다.
-                </p>
-                <p>
-                  📄 파일 분석 및 시각화 엑셀과 같은 파일을 제공하면 원하는
-                  부분에 대한 질문을 해보세요. 문서 파일을 첨부하면 해당 문서에
-                  대한 질문응답도 가능합니다. 오른쪽 패널에서 시각화 결과가 즉시
-                  표시됩니다. 예시: "2023년 가장 높은 판매량을 기록한 달은 몇
-                  월인가요?"
-                </p>
-                <p>
-                  📊 데이터 분석 및 인사이트: 데이터에 대한 분석을 요청하면
-                  비버가 분석 결과와 함께 인사이트를 제공합니다. 제공된 파이썬
-                  코드를 통해 직접 검증이 가능합니다.
-                </p>
-                <p>
-                  비버를 통해 문서 파일에 대한 질문응답과 데이터 시각화, 분석을
-                  통한 풍부한 정보를 얻어보세요!
-                </p>
-              </div>
-            )
-          }
+        <div className="flex justify-center">
+          {currentState === "init" && <InitialGuide />}
         </div>
         {/* 채팅 메시지 출력 */}
         <ChatLogs />
---
+++ src/components/main/Chat/InitialGuide.js
@@ -0,0 +1,58 @@
+import beaver from "../../../image/logo.jpg";
+
+function InitialGuide() {
+  return (
+    <div className="aspect-x-16 aspect-h-9 mx-10 my-4 px-10 py-4 bg-[#EFEFEE] rounded-[50px] font-['BMHANNAPro'] shadow-[9px_9px_6px_0px_rgba(0,0,0,0.25)]">
+      <p className="font-bold flex justify-center items-center text-[50px]">
+        <img src={beaver} className="h-20 w-20 rounded-full mr-4" />
+        비버.ai 사용법
+      </p>
+
+      <div className="">
+        <p className="text-lg font-bold my-4">
+          비버는 일반적인 챗봇🤖 기능을 제공하며, 파일을 첨부하면 파일 종류에
+          따라 질문응답이 가능합니다.
+        </p>
+
+        <ul className="mb-4 list-disc list-outside">
+          <div className="font-bold flex justify-start items-center">
+            <div className="text-[30px]">📄</div>
+            파일 분석 및 시각화
+          </div>
+          <li className="ml-10">
+            엑셀과 같은 파일을 제공하면 원하는 부분에 대한 질문을 해보세요.
+          </li>
+          <li className="ml-10">
+            문서 파일을 첨부하면 해당 문서에 대한 질문응답도 가능합니다.
+          </li>
+          <li className="ml-10">
+            오른쪽 패널에서 시각화 결과가 즉시 표시됩니다.
+          </li>
+          <li className="italic ml-10">
+            예시: "2023년 가장 높은 판매량을 기록한 달은 몇 월인가요?"
+          </li>
+        </ul>
+
+        <ul className="mb-4 list-disc list-outside">
+          <div className="font-bold flex justify-start items-center">
+            <div className="text-[30px]">📊</div>
+            데이터 분석 및 인사이트
+          </div>
+          <li className="ml-10">
+            데이터에 대한 분석을 요청하면 비버가 분석 결과와 함께 인사이트를
+            제공합니다.
+          </li>
+          <li className="ml-10">
+            제공된 파이썬 코드를 통해 직접 검증이 가능합니다.
+          </li>
+        </ul>
+
+        <p className="mt-4">
+          비버를 통해 문서 파일에 대한 질문응답과 데이터 시각화, 분석을 통한
+          풍부한 정보를 얻어보세요!
+        </p>
+      </div>
+    </div>
+  );
+}
+export default InitialGuide;
