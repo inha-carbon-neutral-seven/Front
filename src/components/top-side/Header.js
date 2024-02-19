223cc667a812ec0b49fdc9b05f3f2ce329775adb
hemaher0 <hemahero@naver.com>
2024-02-19 16:34:49 +0900

    [강민우] 다크 모드 초기

--- src/components/top-side/Header.js
+++ src/components/top-side/Header.js
@@ -1,21 +1,14 @@
 import ServerStatusIndicator from "./ServerStateIndicator";
-import Button from "@mui/material/Button";
 import beaver from "../../image/logo.jpg";
 
-function Header({ param }) {
-  const handleButtonClick = (p) => {
-    param(p);
-  };
-
+function Header() {
   return (
-    <div className="z-50 h-16 bg-gradient-to-r from-beaver-2 to-beaver-1 flex justify-between items-center px-4 w-4/5 fixed rounded-[12px] drop-shadow-lg place-self-center mt-3">
+    <div className="z-50 h-16 bg-gradient-to-r from-gray-700 to-gray-900 dark:from-gray-200 dark:to-gray-300 flex justify-between items-center px-4 w-4/5 fixed rounded-[12px] drop-shadow-lg place-self-center mt-3">
       <div className="flex items-center space-x-4">
         <div className="flex items-center">
           <a href="/" className="flex items-center">
             <img src={beaver} className="h-12 mr-3 rounded-full" />
-            <span className="self-center text-xl font-extrabold sm:text-2xl whitespace-nowrap text-white">
-              비버.ai
-            </span>
+            <span className="self-center text-xl font-extrabold sm:text-2xl whitespace-nowrap dark:text-gray-900 text-white">비버.ai</span>
           </a>
         </div>
       </div>
